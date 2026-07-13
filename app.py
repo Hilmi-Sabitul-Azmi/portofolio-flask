8e2c8e237d8770196ee3be76c24030c38f0aa44b
Hilmi-Sabitul-Azmi <hilmi170717@gmail.com>
2026-07-13 15:24:55 +0700

    Setup koneksi database, model Project & Message, dan update gitignore

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+from extensions import db
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kunci-rahasia-anda'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portofolio.db'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 
-@app.route('/')
-def home():
-    return "Halo"
+db.init_app(app)
+
+from models import Project, Message
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,21 @@
+from extensions import db
+from datetime import datetime
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    technologies = db.Column(db.String(200))
+    image_file = db.Column(db.String(120), default='default.jpg')
+    github_link = db.Column(db.String(200))
+    live_link = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    is_read = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
