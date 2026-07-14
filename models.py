fc8bdf32036a0b9598b8e902777e0b8b1349eabf
Hilmi-Sabitul-Azmi <hilmi170717@gmail.com>
2026-07-14 11:08:02 +0700

    Tambah model Education dan tampilkan data dinamis di halaman About

--- models.py
+++ models.py
@@ -27,7 +27,6 @@ class Profile(db.Model):
     name = db.Column(db.String(100), nullable=False)
     headline = db.Column(db.String(150))
     about = db.Column(db.Text)
-    education = db.Column(db.Text)
     photo = db.Column(db.String(120), default='default-profile.jpg')
     email = db.Column(db.String(120))
     github = db.Column(db.String(200))
@@ -38,4 +37,13 @@ class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     level = db.Column(db.String(50))
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
+
+
+class Education(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    institution = db.Column(db.String(150), nullable=False)
+    degree = db.Column(db.String(150))
+    year_start = db.Column(db.String(50))
+    year_end = db.Column(db.String(50))
     profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
